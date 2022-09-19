a9e755a7195fd7703cbdf0cfde7459bcbb8d8ae5
Tyler <toygrrr@gmail.com>
2022-09-19 10:08:28 -0700

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,21 @@
 class CheesesController < ApplicationController
 
+
+  #neat way to add conditional logic to URL and use things other than ID
   def index
-    cheeses = Cheese.all
+    if params[:is_best_seller] == "true"
+      cheeses = Cheese.where(is_best_seller: true)
+    elsif params[:is_best_seller] == "false"
+      cheeses = Cheese.where(is_best_seller: false)
+    else
+      cheeses = Cheese.all
+    end
     render json: cheeses
+  end
+
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
   end
 
 end
